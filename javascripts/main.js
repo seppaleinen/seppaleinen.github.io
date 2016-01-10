8d6b3bf5249b383cb058520d555e1c40a2f4a957
David Eriksson <davideriksson@swedenmail.com>
2016-01-10 02:00:22 +0100

    Added project includes with buttons

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,5 @@
 console.log('This would be the main JS file.');
+$( "button" ).click(function() {
+  $( "#section_" + this.id ).toggleClass( 'hidden' );
+  $( "#" + this.id ).toggleClass('active')
+});
