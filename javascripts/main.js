c92ec7a1db938a12baa88445ce2b9f44f252861a
David Eriksson <davideriksson@swedenmail.com>
2016-01-13 22:35:23 +0100

    Meckelimeck

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,22 @@
+'use strict';
+
+// Logic for language-buttons
 $( "button" ).click(function() {
-  $( "#section_" + this.id ).toggleClass( 'hidden' );
-  $( "#" + this.id ).toggleClass('active')
+    $( "#section_" + this.id ).toggleClass( 'hidden' );
+    $( "#" + this.id ).toggleClass('active');
 });
+
+/*
+// Logic for flipping between pages
+$( ".navbar-nav > li > a" ).click(function() {
+    if( this.id != "index.html" || this.id != "github" ) {
+        $( "#content" ).load( this.id );
+    }
+});
+
+// Activating/Inactivating nav-bar links
+$( ".navbar-nav > li" ).click(function() {
+    $( ".navbar-nav > li" ).removeClass("active");
+    $( this ).addClass("active");
+});
+*/
